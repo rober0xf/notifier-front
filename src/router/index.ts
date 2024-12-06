28a30cdb0e0eed22085bc8f40c0786c9ec6ca8f4
kewsor0x <kewsor0x@gmail.com>
2024-12-06 15:08:02 -0300

    init

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HelloWorld from "../components/HelloWorld.vue";
+import LoginView from "../views/LoginView.vue";
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HelloWorld
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView
+        },
+    ]
+})
+
+export default router;

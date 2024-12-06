5c37db4d2bdfaa3b8dae64f59423ae5f998f2834
kewsor0x <kewsor0x@gmail.com>
2024-12-06 15:06:31 -0300

    login template

---
+++ src/views/LoginView.vue
@@ -0,0 +1,24 @@
+/** functionality */
+<script setup lang="ts">
+import { useLogin } from '../composables/useLogin';
+
+const { identity, password, errorMessage, login } = useLogin();
+</script>
+
+/** templates */
+<template>
+    <main>
+        <h1>Login</h1>
+        <form @submit.prevent="login">
+            <!-- v-model is there reference to the variables that has the ref function -->
+            <label for="identity">Email</label>
+            <input v-model="identity" type="identity" id="identity" name="identity" required />
+            <label for="password">Password</label>
+            <input v-model="password" type="password" id="password" name="password" required />
+            <button type="submit">Login</button>
+            <p v-if="errorMessage">{{ errorMessage }}</p>
+        </form>
+    </main>
+</template>
+
+/** styles */

1c8e5576a0e0436f12b9d1bf5c97cce4bebbd5c7
rober0xf <robertinopili13@gmail.com>
2025-09-11 01:39:06 -0300

    dashboard view

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+
+defineComponent({
+  name: 'DashboardView',
+})
+</script>
+
+<template>
+  <div>
+    <h1>Dashboard</h1>
+  </div>
+</template>

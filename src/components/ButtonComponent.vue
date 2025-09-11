8c13e7e83f4b9480b1c8590e8eca8227fdd6a2a6
rober0xf <robertinopili13@gmail.com>
2025-09-11 01:37:53 -0300

    components: botton

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+  type?: 'submit' | 'button' | 'reset'
+  label: string
+}>()
+
+const buttonType = props.type ?? 'submit'
+</script>
+
+<template>
+  <button :type="buttonType">{{ props.label }}</button>
+</template>

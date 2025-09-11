7ba879e053d8674c0abb874ff41545c724fd841c
rober0xf <robertinopili13@gmail.com>
2025-09-11 01:38:11 -0300

    components: title

---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TitleComponent',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<template>
+  <h1 class="text-4xl font-extrabold text-gray-800 tracking-wide leading-tight">{{ title }}</h1>
+</template>

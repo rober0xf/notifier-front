0822588583777b7733aa68c08353f543d9f33530
rober0xf <robertinopili13@gmail.com>
2025-09-11 01:39:32 -0300

    login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,54 +1,60 @@
-<script lang="ts">
-import { defineComponent, ref } from 'vue'
+<script setup lang="ts">
 import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import InputComponent from '@/components/InputComponent.vue'
+import ButtonComponent from '@/components/ButtonComponent.vue'
 
-export default defineComponent({
-  name: 'LoginView',
-  setup() {
-    const username = ref('')
-    const password = ref('')
-    const errorMessage = ref('')
-    const router = useRouter()
+const username = ref('')
+const password = ref('')
+const errorMessage = ref('')
+const router = useRouter()
 
-    const login = async () => {
-      try {
-        const response = await fetch('/api/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ username: username.value, password: password.value }),
-        })
-        if (response.ok) {
-          const data = await response.json()
-          localStorage.setItem('token', data.token)
-          router.push({ name: 'Home' })
-        } else {
-          errorMessage.value = 'Invalid username or password.'
-        }
-      } catch (error) {
-        errorMessage.value = 'An error occurred while logging in.'
-      }
+const login = async () => {
+  try {
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: username.value, password: password.value }),
+    })
+    if (response.ok) {
+      const res = await response.json()
+      localStorage.setItem('token', res.data)
+      router.push({ name: 'Home' })
+    } else {
+      errorMessage.value = 'Invalid username or password.'
     }
-    return {
-      username,
-      password,
-      errorMessage,
-      login,
-    }
-  },
-})
+  } catch {
+    errorMessage.value = 'An error occurred while logging in.'
+  }
+}
 </script>
 
 <template>
   <main>
     <h1>Login</h1>
     <form @submit.prevent="login">
-      <label for="username">Username</label>
-      <input for="username" id="username" name="username" required />
-      <label for="password">Password</label>
-      <input for="password" type="password" id="password" name="password" required />
-      <button type="submit">Log In</button>
+      <InputComponent
+        v-model="username"
+        type="text"
+        id="username"
+        name="username"
+        label="Username"
+        required
+      />
+      <InputComponent
+        v-model="password"
+        for="password"
+        id="password"
+        name="password"
+        type="password"
+        label="Password"
+        required
+      />
+
+      <ButtonComponent type="submit" label="Log In" />
+      <p v-if="errorMessage">{{ errorMessage }}</p>
     </form>
   </main>
 </template>

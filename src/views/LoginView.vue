1f3f73a616f03ce78e00e0ba2c5aa8eba31144b8
rober0xf <robertinopili13@gmail.com>
2025-09-16 02:59:12 -0300

    login component & login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,60 +1,66 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 import { ref } from 'vue'
-import InputComponent from '@/components/InputComponent.vue'
-import ButtonComponent from '@/components/ButtonComponent.vue'
+import LoginComponent from '@/components/LoginComponent.vue'
 
-const username = ref('')
+const email = ref('')
 const password = ref('')
 const errorMessage = ref('')
+const isLoading = ref(false)
 const router = useRouter()
 
+// here we use the variables from the parent component
+const updateEmail = (value: string) => {
+  email.value = value
+}
+
+const updatePassword = (value: string) => {
+  password.value = value
+}
+
 const login = async () => {
+  if (!email.value || !password.value) {
+    errorMessage.value = 'fields missing!'
+    return
+  }
+
+  isLoading.value = true
+  errorMessage.value = ''
+
   try {
     const response = await fetch('/api/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ username: username.value, password: password.value }),
+      body: JSON.stringify({ email: email.value, password: password.value }),
     })
+    console.log('Response:', response)
     if (response.ok) {
       const res = await response.json()
       localStorage.setItem('token', res.data)
       router.push({ name: 'Home' })
     } else {
-      errorMessage.value = 'Invalid username or password.'
+      errorMessage.value = 'Invalid email or password.'
     }
   } catch {
     errorMessage.value = 'An error occurred while logging in.'
+  } finally {
+    isLoading.value = false
   }
 }
 </script>
 
 <template>
-  <main>
-    <h1>Login</h1>
-    <form @submit.prevent="login">
-      <InputComponent
-        v-model="username"
-        type="text"
-        id="username"
-        name="username"
-        label="Username"
-        required
-      />
-      <InputComponent
-        v-model="password"
-        for="password"
-        id="password"
-        name="password"
-        type="password"
-        label="Password"
-        required
-      />
-
-      <ButtonComponent type="submit" label="Log In" />
-      <p v-if="errorMessage">{{ errorMessage }}</p>
-    </form>
+  <main style="background-image: linear-gradient(115deg, #000000, #0f3d0f)">
+    <LoginComponent
+      :email="email"
+      :password="password"
+      :isLoading="isLoading"
+      :errorMessage="errorMessage"
+      @update:email="updateEmail"
+      @update:password="updatePassword"
+      @submit="login"
+    />
   </main>
 </template>

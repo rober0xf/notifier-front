031599f22d52bfa28de8a03234a50586c0876872
rober0xf <robertinopili13@gmail.com>
2025-09-18 03:55:55 -0300

    login component & view

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import ButtonComponent from './ButtonComponent.vue'
 import InputComponent from './InputComponent.vue'
+import LoginImage from '@/images/hero.jpg'
 
 defineOptions({
   name: 'LoginComponent',
@@ -30,70 +31,60 @@ const updatePassword = (value: string) => {
 </script>
 
 <template>
-  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8" style="background-image: linear-gradient(115deg, #000000, #0f3d0f)">
-    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Notifier" class="mx-auto h-10 w-auto" />
-      <h2 class="mt-10 text-center text-2xl/9 font-medium tracking-tight text-gray-300">Sign in to your account</h2>
-    </div>
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form @submit.prevent="handleSubmit" class="space-y-6">
-        <div v-if="errorMessage" class="rounded-md bg-red-50 p-4">
-          <div class="text-sm font-medium text-red-700">
-            {{ errorMessage }}
-          </div>
-        </div>
+  <div class="relative flex w-screen flex-col items-center justify-center">
+    <div class="relative mt-16 flex w-[30%] flex-col items-center gap-4 rounded-md border-2 border-gray-700 px-4 py-6 text-center text-white">
+      <h1 class="text-2xl font-medium text-gray-700 uppercase">Login Page</h1>
 
-        <div>
-          <label for="email" class="block text-sm/6 font-medium text-gray-300">Email address</label>
-          <div class="mt-2">
-            <InputComponent
-              :modelValue="email"
-              @update:modelValue="updateEmail"
-              id="email"
-              name="email"
-              type="email"
-              placeholder="Enter your email"
-              class="block w-full rounded-md bg-white/5 px-3 py-1.5 font-medium text-gray-300 outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-green-700 sm:text-sm/6"
-              required
-            />
-          </div>
-        </div>
+      <!-- email field-->
+      <div class="mx-auto flex w-[60%] flex-col gap-2">
+        <label for="email" class="text-sm font-medium text-gray-700 underline">Email</label>
+        <InputComponent
+          :modelValue="email"
+          class="rounded-full border-2 bg-gray-200 px-4 py-2 font-semibold text-gray-900 outline-none"
+          id="email"
+          label="Email"
+          type="email"
+          placeholder="Enter your email"
+          @update:modelValue="updateEmail"
+        />
+      </div>
 
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm/6 font-medium text-gray-100">Password</label>
-            <div class="text-sm">
-              <a href="#" class="font-medium text-green-600 hover:text-green-700">Forgot password?</a>
-            </div>
-          </div>
-          <div class="mt-2">
-            <InputComponent
-              :modelValue="password"
-              @update:modelValue="updatePassword"
-              id="password"
-              name="password"
-              type="password"
-              placeholder="Enter your password"
-              class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-green-700 sm:text-sm/6"
-              required
-            />
-          </div>
-        </div>
+      <!-- password field-->
+      <div class="mx-auto flex w-[60%] flex-col gap-2">
+        <label for="password" class="text-sm font-medium text-gray-700 underline">Password</label>
+        <InputComponent
+          :modelValue="password"
+          class="rounded-full border-2 bg-gray-200 px-4 py-2 font-semibold text-gray-900 outline-none"
+          id="password"
+          label="Password"
+          type="password"
+          placeholder="Enter your password"
+          @update:modelValue="updatePassword"
+        />
+      </div>
+      <div class="mb-5">
+        <a href="#" class="font-medium text-gray-900 hover:underline">Forgot password? </a>
+      </div>
 
-        <div>
-          <ButtonComponent
-            type="submit"
-            :label="props.isLoading ? 'Logging in...' : 'Log in'"
-            :disabled="props.isLoading"
-            class="flex w-full cursor-pointer justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm/6 font-medium text-white hover:bg-green-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-700"
-          />
-        </div>
-      </form>
+      <!-- login button -->
+      <ButtonComponent
+        label="Login"
+        :disabled="isLoading"
+        class="relative mx-auto flex cursor-pointer flex-col gap-4 rounded-md border-2 bg-[#fcc61d] px-4 py-2 text-center text-xl text-gray-900 shadow-xl shadow-gray-400 transition-colors duration-300 hover:bg-yellow-500 sm:w-[150px] md:w-[200px]"
+        @click="handleSubmit"
+      />
 
-      <p class="mt-10 text-center text-sm/6 font-medium text-gray-300">
+      <!-- error message if something is wrong -->
+      <div v-if="errorMessage" class="mt-2 text-sm font-bold text-red-500 select-none">
+        {{ errorMessage }}
+      </div>
+
+      <!-- create account -->
+      <p class="mt-4 text-sm font-medium text-gray-700">
         Not a user?
-        <a href="/register" class="font-medium text-green-600 hover:text-green-700">Create an account</a>
+        <a href="/register" class="font-medium text-blue-600 hover:underline"> Create an account </a>
       </p>
     </div>
+    <img :src="LoginImage" alt="Logo" class="-mt-25 w-auto items-center object-contain select-none" />
   </div>
 </template>
